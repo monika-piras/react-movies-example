e0b4bfe96f4cd40036b238174fc999e8eb9cc58c
Monica Piras <monica.piras.info@gmail.com>
2023-05-08 10:22:31 +0200

    back to home button

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 
 const MovieDetail = () => {
   const [movie, setMovie] = useState({});
 
   let { id } = useParams();
 
+  const navigate = useNavigate();
+
+
   useEffect(() => {
     getMovieDetails();
   }, []);
 
   function getMovieDetails() {
-    // fetch(`https://api.tvmaze.com/shows/${id}`)
     fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`)
       .then(response => response.json())
       .then(data => {
         setMovie(data);
         console.log(data);
       })
+  }
+
+  function goBack() {
+    navigate(`/`);
   }
 
   if (!movie.name) {
@@ -37,7 +43,7 @@ const MovieDetail = () => {
       const actors = movie._embedded.cast;
       const renderList = actors.map((item, index) => {
         return (
-          <div key={index}>
+          <div key={index} className='dib'>
             <img alt='movie' src={item.person.image?.medium} />
             <h3>{item.person.name}</h3>
             <p>as: {item.character.name}</p>
@@ -56,26 +62,31 @@ const MovieDetail = () => {
     }
 
     return (
+      <div>
+        <button className='ma3 pa2 bg-light-green br3' onClick={() => goBack()}>
+          <span class="material-symbols-outlined">
+            arrow_back_ios
+          </span>Go Back</button>
 
-      <div className='bg-light-green dib br3 pa3 ma5 bw2 shadow-5'>
+        <div className='bg-light-green dib br3 pa3 ma4 bw2 shadow-5'>
 
-        <div className='db'>
-          <div className='fl mr3'>
-            <img alt='movie' src={movie.image.medium} />
+          <div className='db'>
+            <div className='fl mr3'>
+              <img alt='movie' src={movie.image.medium} />
+            </div>
+            <div className='pa3'>
+              <h1>{movie.name}</h1>
+              <p>{newStr}</p>
+              <p>Language: {movie.language}</p>
+              <h3>{movie.genres.join(', ')}</h3>
+            </div>
           </div>
-          <div className='pa3'>
-            <h1>{movie.name}</h1>
-            <p>{newStr}</p>
-            <p>Language: {movie.language}</p>
-            <h3>{movie.genres.join(', ')}</h3>
-          </div>
-        </div>
 
-        <div className='db'>
-          {rendercast()}
+          <div className='db'>
+            {rendercast()}
+          </div>
         </div>
       </div>
-
     )
   }
 
